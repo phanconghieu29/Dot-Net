b4ee333850664378419e9272630f5d97fcfbc429
unknown <phanconghieu12@gmail.com>
2023-08-30 08:45:58 +0700

    first commit

---
+++ Bai3_GiaoDien/Bai1/Bai1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Bai1
+{
+    partial class Bai1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbA = new System.Windows.Forms.Label();
+            this.lbB = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lbKQ = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbA.Location = new System.Drawing.Point(22, 35);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(25, 13);
+            this.lbA.TabIndex = 0;
+            this.lbA.Text = "a =";
+            // 
+            // lbB
+            // 
+            this.lbB.AutoSize = true;
+            this.lbB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbB.Location = new System.Drawing.Point(239, 35);
+            this.lbB.Name = "lbB";
+            this.lbB.Size = new System.Drawing.Size(25, 13);
+            this.lbB.TabIndex = 2;
+            this.lbB.Text = "b =";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(60, 28);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(134, 20);
+            this.txtA.TabIndex = 1;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            this.txtA.Leave += new System.EventHandler(this.txtA_Leave);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(270, 28);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(135, 20);
+            this.txtB.TabIndex = 3;
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            this.txtB.Leave += new System.EventHandler(this.txtB_Leave);
+            // 
+            // lbKQ
+            // 
+            this.lbKQ.AutoSize = true;
+            this.lbKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKQ.Location = new System.Drawing.Point(22, 69);
+            this.lbKQ.Name = "lbKQ";
+            this.lbKQ.Size = new System.Drawing.Size(51, 13);
+            this.lbKQ.TabIndex = 4;
+            this.lbKQ.Text = "Kết quả";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(79, 62);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(326, 20);
+            this.txtKQ.TabIndex = 5;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCong.Location = new System.Drawing.Point(12, 95);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 33);
+            this.btnCong.TabIndex = 6;
+            this.btnCong.Text = "+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTru.Location = new System.Drawing.Point(119, 95);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(75, 33);
+            this.btnTru.TabIndex = 7;
+            this.btnTru.Text = "-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhan.Location = new System.Drawing.Point(231, 95);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(75, 33);
+            this.btnNhan.TabIndex = 8;
+            this.btnNhan.Text = "*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChia.Location = new System.Drawing.Point(330, 95);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(75, 33);
+            this.btnChia.TabIndex = 9;
+            this.btnChia.Text = "/";
+            this.btnChia.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 155);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.lbKQ);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lbB);
+            this.Controls.Add(this.lbA);
+            this.Name = "Bai1";
+            this.Text = "Cộng trừ nhân chia";
+            this.Leave += new System.EventHandler(this.btnTru_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.Label lbB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lbKQ;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Bai3_GiaoDien/Bai1/Bai1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtA_Leave(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if(ctr.Text.Trim().Length == 0)
+            {
+                this.errorProvider1.SetError(ctr, "Bạn chưa nhập a!");
+            }
+            else
+            {
+                this.errorProvider1.Clear();
+            }
+        }
+
+        private void txtB_Leave(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+            {
+                this.errorProvider1.SetError(ctr, "Bạn chưa nhập b!");
+            }
+            else
+            {
+                this.errorProvider1.Clear();
+            }
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int kq = a + b;
+
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int kq = a - b;
+
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int kq = a + b;
+
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if(ctr.Text.Length > 0 && !Char.IsDigit(ctr.Text[ctr.Text.Length-1]))
+            {
+                this.errorProvider1.SetError(ctr, "Đây không phải số hợp lệ");
+            }
+            else
+            {
+                this.errorProvider1.Clear();
+            }
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Length > 0 && !Char.IsDigit(ctr.Text[ctr.Text.Length - 1]))
+            {
+                this.errorProvider1.SetError(ctr, "Đây không phải số hợp lệ");
+            }
+            else
+            {
+                this.errorProvider1.Clear();
+            }
+        }
+    }
+}
