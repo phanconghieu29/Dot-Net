4c5d2fc59fdcb8a132fa541c1fb2caf6c584afda
Phan Cong Hieu <phanconghieu12@gmail.com>
2023-09-01 21:58:57 +0700

    update

--- Bai3_GiaoDien/Bai1/Bai4.Designer.cs
+++ Bai3_GiaoDien/Bai1/Bai4.Designer.cs
@@ -28,24 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.Label();
+            this.lbSum = new System.Windows.Forms.Label();
+            this.lbSumEven = new System.Windows.Forms.Label();
+            this.lbSumOdd = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.txtSumEven = new System.Windows.Forms.TextBox();
+            this.txtSumOdd = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lbTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(56, 24);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(452, 39);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Nhập Dãy Số và Tính Tổng";
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInput.Location = new System.Drawing.Point(30, 89);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(105, 26);
+            this.lbInput.TabIndex = 1;
+            this.lbInput.Text = "Nhập số :";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.AutoSize = true;
+            this.lbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOutput.Location = new System.Drawing.Point(30, 136);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(158, 26);
+            this.lbOutput.TabIndex = 2;
+            this.lbOutput.Text = "Dãy vừa nhập :";
+            // 
+            // lbSum
+            // 
+            this.lbSum.AutoSize = true;
+            this.lbSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSum.Location = new System.Drawing.Point(30, 182);
+            this.lbSum.Name = "lbSum";
+            this.lbSum.Size = new System.Drawing.Size(190, 26);
+            this.lbSum.TabIndex = 3;
+            this.lbSum.Text = "Tổng các phần tử :";
+            // 
+            // lbSumEven
+            // 
+            this.lbSumEven.AutoSize = true;
+            this.lbSumEven.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSumEven.Location = new System.Drawing.Point(30, 225);
+            this.lbSumEven.Name = "lbSumEven";
+            this.lbSumEven.Size = new System.Drawing.Size(130, 26);
+            this.lbSumEven.TabIndex = 4;
+            this.lbSumEven.Text = "Tổng Chẵn :";
+            // 
+            // lbSumOdd
+            // 
+            this.lbSumOdd.AutoSize = true;
+            this.lbSumOdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSumOdd.Location = new System.Drawing.Point(303, 225);
+            this.lbSumOdd.Name = "lbSumOdd";
+            this.lbSumOdd.Size = new System.Drawing.Size(102, 26);
+            this.lbSumOdd.TabIndex = 5;
+            this.lbSumOdd.Text = "Tổng Lẻ :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(141, 94);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(264, 22);
+            this.txtInput.TabIndex = 6;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(194, 141);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(348, 22);
+            this.txtOutput.TabIndex = 7;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(226, 187);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(316, 22);
+            this.txtSum.TabIndex = 8;
+            // 
+            // txtSumEven
+            // 
+            this.txtSumEven.Location = new System.Drawing.Point(166, 230);
+            this.txtSumEven.Name = "txtSumEven";
+            this.txtSumEven.Size = new System.Drawing.Size(131, 22);
+            this.txtSumEven.TabIndex = 9;
+            // 
+            // txtSumOdd
+            // 
+            this.txtSumOdd.Location = new System.Drawing.Point(411, 230);
+            this.txtSumOdd.Name = "txtSumOdd";
+            this.txtSumOdd.Size = new System.Drawing.Size(131, 22);
+            this.txtSumOdd.TabIndex = 10;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(439, 78);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(103, 44);
+            this.btnInput.TabIndex = 11;
+            this.btnInput.Text = "Nhập";
+            this.btnInput.UseVisualStyleBackColor = true;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Location = new System.Drawing.Point(141, 258);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(103, 44);
+            this.btnContinue.TabIndex = 12;
+            this.btnContinue.Text = "Tiếp Tục";
+            this.btnContinue.UseMnemonic = false;
+            this.btnContinue.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(329, 258);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 44);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
             // Bai4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(565, 321);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.txtSumOdd);
+            this.Controls.Add(this.txtSumEven);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lbSumOdd);
+            this.Controls.Add(this.lbSumEven);
+            this.Controls.Add(this.lbSum);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.lbInput);
+            this.Controls.Add(this.lbTitle);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Bai4";
             this.Text = "Bai4";
             this.ResumeLayout(false);
@@ -55,6 +197,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Label lbOutput;
+        private System.Windows.Forms.Label lbSum;
+        private System.Windows.Forms.Label lbSumEven;
+        private System.Windows.Forms.Label lbSumOdd;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.TextBox txtSumEven;
+        private System.Windows.Forms.TextBox txtSumOdd;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Button btnExit;
     }
 }
