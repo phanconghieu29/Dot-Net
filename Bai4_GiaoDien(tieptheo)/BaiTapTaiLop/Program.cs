ce862c7a01527ee97ba7fb805233d8bcb54cff74
Phan Cong Hieu <phanconghieu12@gmail.com>
2023-09-14 20:30:06 +0700

    update

--- Bai4_GiaoDien(tieptheo)/BaiTapTaiLop/Program.cs
+++ Bai4_GiaoDien(tieptheo)/BaiTapTaiLop/Program.cs
@@ -16,7 +16,7 @@ namespace BaiTapTaiLop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Bai2());
+            Application.Run(new Bai1());
         }
     }
 }
