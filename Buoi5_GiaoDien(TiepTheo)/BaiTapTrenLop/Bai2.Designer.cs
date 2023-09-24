a1ae496e04f9a10ece84c0637e9b6b16270794fe
Phan Cong Hieu <phanconghieu12@gmail.com>
2023-09-24 19:10:08 +0700

    update

---
+++ Buoi5_GiaoDien(TiepTheo)/BaiTapTrenLop/Bai2.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace BaiTapTrenLop
+{
+    partial class Bai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            treeView1 = new TreeView();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            txt_Address = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            label7 = new Label();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(-1, 0);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(305, 438);
+            treeView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(447, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(315, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(362, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Chọn Lớp";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(325, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Thông Tin Sinh Viên";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(362, 93);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Mã SV";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(362, 147);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Họ Tên";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(447, 90);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(315, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(447, 144);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(315, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(362, 204);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Địa Chỉ";
+            // 
+            // txt_Address
+            // 
+            txt_Address.Location = new Point(447, 201);
+            txt_Address.Name = "txt_Address";
+            txt_Address.Size = new Size(315, 27);
+            txt_Address.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(447, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 29);
+            button1.TabIndex = 12;
+            button1.Text = "Cập Nhật";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(614, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(148, 29);
+            button2.TabIndex = 13;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(325, 295);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(94, 24);
+            checkBox1.TabIndex = 14;
+            checkBox1.Text = "Thêm lớp";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(447, 362);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(184, 27);
+            textBox3.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(362, 365);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Địa Chỉ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(325, 331);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 20);
+            label7.TabIndex = 17;
+            label7.Text = "Thông Tin Lớp";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(637, 361);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 29);
+            button3.TabIndex = 18;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Bai2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 439);
+            Controls.Add(button3);
+            Controls.Add(label7);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txt_Address);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(treeView1);
+            Name = "Bai2";
+            Text = "Bai2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TreeView treeView1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox txt_Address;
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label7;
+        private Button button3;
+    }
+}
